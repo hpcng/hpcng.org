0a00c5ade28cedfd761d535cf64ee660574fab0a
Michael Kinder <michael@hostbend.net>
2021-07-23 15:33:53 -0400

    add about page to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,29 +4,37 @@ import { Link } from "gatsby"
 const Footer = () => {
   const navigation = {
     main: [
-      { name: 'News', href: '/news' },
-      { name: 'Warewulf', href: 'https://warewulf.hpcng.org' },
-      { name: 'Singularity', href: 'https://singularity.hpcng.org' },
+      { name: "About", href: "/about" },
+      { name: "News", href: "/news" },
+      { name: "Warewulf", href: "https://warewulf.hpcng.org" },
+      { name: "Singularity", href: "https://singularity.hpcng.org" },
     ],
   }
-
 
   return (
     <footer className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-          {navigation.main.map((item) => (
+        <nav
+          className="-mx-5 -my-2 flex flex-wrap justify-center"
+          aria-label="Footer"
+        >
+          {navigation.main.map(item => (
             <div key={item.name} className="px-5 py-2">
-              <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+              <a
+                href={item.href}
+                className="text-base text-gray-500 hover:text-gray-900"
+              >
                 {item.name}
               </a>
             </div>
           ))}
         </nav>
-        <p className="mt-8 text-center text-base text-gray-400">&copy; 2021 HPCng. All rights reserved.</p>
+        <p className="mt-8 text-center text-base text-gray-400">
+          &copy; 2021 HPCng. All rights reserved.
+        </p>
       </div>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
