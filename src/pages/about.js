24d847d3f656f928cbce7060caed643a74302b83
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2021-08-31 15:02:38 -0500

    update link to community charter and remove redundant background

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,21 +24,6 @@ const AboutPage = () => (
         <div className="relative">
           <div className="relative md:bg-white md:p-6">
             <div className="lg:grid lg:grid-cols-2 lg:gap-6">
-              <div className="prose prose-green prose-lg text-gray-500 lg:max-w-none">
-                <h1>Background</h1>
-                <p>
-                  High-Performance Computing (HPC), a once niche use-case, has been growing in terms of applicability over the last 5-10 years. We are seeing large scale, tightly integrated HPC resources beginning to run non-traditional HPC workloads in what we commonly refer to as "the long tail of scientific computing". As that long tail is growing, it is becoming increasingly common for new applications, use-cases, and science domains to be making use of traditional high-performance computing infrastructures. Additionally, HPC environments are not being used only in science, adoption is growing into the enterprise via the advent of artificial intelligence training as well as compute and data-driven analytics.
-                </p>
-                <p>
-                  Today when we refer to HPC, we no longer are referring only to those tightly coupled parallel MPI based applications. We are referring to any applications, or series of applications, which are designed to run a given workload as fast as the hardware will allow. This usually means that the performance-critical application will utilize a given subsystem of a hardware stack completely, whether that be CPU, memory, storage, network, GPU, PCI, etc. This could be on a single compute node, a massive GPU workhorse, CPU farm, or a tightly coupled parallel processing infrastructure. For the purpose of this charter, HPC inclusively refers to all of these types of workloads as HPC.
-                </p>
-                <p>
-                  So while "HPC" is becoming increasingly diverse, the architecture to which it is built has not changed considerably over the past two and a half decades. This base architecture, commonly referred to as the "Beowulf" design, is predisposed to being flat, isolated and discrete which has imposed difficulties with the integration of non-HPC specific capabilities.
-                </p>
-                <p>
-                  This divide has been increasingly causing separation as enterprise architectures, like micro-services, containers, orchestration, etc., have been readily innovating over the past decade and creating a lot of opportunities for optimization of development, operations, security, automation, reliability, and scale. But these new capabilities are not easily transcribed into the HPC sector due to misalignments of base architectures and thus cross-pollination is not occurring. This leads to the replication of technology and a lack of benefit of experience and capabilities for everyone. HPCng strives to build a community of diverse brilliant people to consider these challenges and work together on solutions.
-                </p>
-              </div>
               <div className="mt-6 prose prose-green prose-lg text-gray-500 lg:mt-0">
                 <h1>Mission</h1>
                 <p>
@@ -51,27 +36,15 @@ const AboutPage = () => (
                   "The convergence of AI, data analytics and traditional simulation will result in systems with broader capabilities and configurability as well as cross pollination." -- Dr. Al Gara, Intel
                 </p>
               </div>
+              <div className="prose prose-green prose-lg text-gray-500 lg:max-w-none">
+                <h1>Community Charter</h1>
+                <p>
+                  The initial <a href=https://github.com/hpcng/community-charter/blob/main/README.md>Community Charter</a> is available and hosted on github.  Please read it and submit issues or pull requests if you have ideas for improvements.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="px-4 bg-gray-100 py-10">
-      <div className="max-w-2xl mb-10 mt-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-        <h2 className="max-w-xl mb-6 font-sans text-4xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-5xl md:mx-auto">
-            Community Charter
-        </h2>
-        <p className="mb-5 text-base text-gray-700 md:text-lg">
-            While the community charter is still in draft, it is updated often, so download it here.
-        </p>
-        <a
-          href="https://docs.google.com/document/d/1UNUawKaU0SlKLdszgKm8LHxyjXDfX87YwuF3zJ29d5U/export?format=pdf"
-          class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-900 hover:bg-green-800 focus:shadow-outline focus:outline-none"
-          aria-label="Download Community Charter"
-          title="Download Community Charter"
-        >
-          Download
-        </a>
       </div>
     </div>
   </Layout>
